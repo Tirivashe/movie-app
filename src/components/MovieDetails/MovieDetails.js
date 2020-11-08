745b5cad363065301b7595565115c791365d4c0d
Tiri <shaymusts@gmail.com>
2020-11-07 18:04:01 -0600

    Added design and functionality to the each movie page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,11 +1,14 @@
+import { Button, Chip, Grid, Typography } from '@material-ui/core'
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { useStyles } from './styles'
 
 function MovieDetails() {
   const [movie, setMovie] = useState({})
   const { id } = useParams()
   const classes = useStyles()
+
+  const image = `https://image.tmdb.org/t/p/w500${movie.poster_path ? movie.poster_path: movie.backdrop_path}`
 
   console.log(movie)
 
@@ -24,10 +27,40 @@ function MovieDetails() {
   },[id])
 
   return (
-    <div className={classes.root}>
-      Selected: {movie.title}
-    </div>
+    <Grid container alignItems="center" justify="center" spacing={3} className={classes.root}>
+      <Typography className={classes.title} variant="h2">{movie.title}</Typography>
+      <Grid className={classes.content} container direction="row" justify="space-between">
+        <Grid item xs={12} md={6}>
+          <img className={classes.image} src={image} alt="poster"/>
+        </Grid>
+        <Grid className={classes.textContent} item container direction="column" xs={12} md={6} spacing={6}>
+          <Grid item component={Typography} variant="h4">
+            Release Date
+            <Typography>{movie.release_date}</Typography>
+          </Grid>
+          <Grid item component={Typography} variant="h4">
+            Runtime
+            <Typography>{movie.runtime} mins</Typography>
+          </Grid>
+          <Grid item component={Typography} variant="h4">
+            Genres
+            <Grid item container alignItems="center">
+              {typeof movie.genres !== "undefined" && movie.genres.map(genre => (
+                <Chip label={genre.name} color="primary"/>
+              ))}
+            </Grid>
+          </Grid>
+          <Grid item component={Typography} variant="h4">
+            Overview
+            <Typography className={classes.overview}>{movie.overview}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Link className={classes.buttonLink} to="/">
+        <Button className={classes.button}>Back To Home</Button>
+      </Link>
+    </Grid>
   )
 }
 
-export default MovieDetails
+export default MovieDetails 
--- src/components/MovieDetails/styles.js
+++ src/components/MovieDetails/styles.js
@@ -2,6 +2,53 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles(theme => ({
   root: {
-    height: "100vh"
+    height: "100vh",
+    width: "100%",
+    padding: "2.5rem",
+    boxSizing: "border-box",
+  },
+
+  content: {
+    
+  },
+
+  textContent: {
+    height: "100%"
+  },
+
+  title: {
+    marginTop: "1rem",
+    marginBottom: "2.5rem",
+  },
+
+  image: {
+    height: "100%"
+  },
+
+  overview: {
+  },
+
+  buttonLink: {
+    margin:"2.5rem 0",
+    textDecoration: "none",
+    width: "15%"
+  },
+
+  button: {
+    backgroundColor: "#522A85",
+    padding: "0.5rem",
+    borderRadius: "2rem",
+    boxShadow: "5px 5px 25px rgba(0,0,0,0.8)",
+    color: "#E73DEB",
+    width: "100%",
+    '&:hover': {
+      color: "#000",
+      backgroundColor: "#fff"
+    }
+  },
+
+  chip: {
+    display: "block"
   }
+  
 }))
